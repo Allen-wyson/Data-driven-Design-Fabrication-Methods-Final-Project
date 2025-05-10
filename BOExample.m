9262bb2b542ebb4789e7678e69113307bfe1ec93
Yo-Shiun Cheng <allenwyson0203@gmail.com>
2025-05-10 19:01:56 +0200

    try some stuff

--- BOExample.m
+++ BOExample.m
@@ -1,27 +1,27 @@
-clc; close all; clear ; 
+clc; close all; clear;
 
-X1 = optimizableVariable('radialThreads', [5 15], 'Type', 'integer');
-X2 = optimizableVariable('spiralThreads', [5 15], 'Type', 'integer');
-X3 = optimizableVariable('dr', [2.5 4]);      % diameter of radial thread
-X4 = optimizableVariable('ds', [0.5 2]);      % diameter of spiral thread
-vars = [X1, X2, X3, X4];
+% Set up design parameters
+X1 = optimizableVariable('x',[1 10]);
+X2 = optimizableVariable('y',[1 20]);
+vars = [X1,X2];
 
 % Perform Bayesian Optimization
-% results = bayesopt(@getdistance,vars,'AcquisitionFunctionName','expected-improvement-plus')
+results = bayesopt(@getdistance,vars,'AcquisitionFunctionName','expected-improvement-plus')
 %results = bayesopt(@getdistance,vars,'AcquisitionFunctionName','expected-improvement-plus', 'ExplorationRatio',0.9, 'NumSeedPoints', 4)
-results = bayesopt(@getdistance,vars,'AcquisitionFunctionName','expected-improvement-plus', 'ExplorationRatio',0.9, 'NumSeedPoints', 4, 'PlotFcn',{@plotAcquisitionFunction, @plotObjectiveModel,@plotMinObjective})
+%results = bayesopt(@getdistance,vars,'AcquisitionFunctionName','expected-improvement-plus', 'ExplorationRatio',0.9, 'NumSeedPoints', 4, 'PlotFcn',{@plotAcquisitionFunction, @plotObjectiveModel,@plotMinObjective})
 
 
 % Function to Optimize
 function fval = getdistance(in)
-    radialThreads = in.radialThreads;
-    spiralThreads = in.spiralThreads;
-    dr = in.dr;
-    ds = in.ds;
-
-    % Example objective (replace with your real one)
-    Z = sin(radialThreads) + cos(spiralThreads) + dr - ds;
-    fval = Z;
+x(1) = in.x;
+x(2) = in.y;
+Z = sin(x(1)) + cos(x(2));
+fval = Z;
 end
 
-
+% %
+% [X,Y] = meshgrid(1:0.5:10,1:20);
+% Z = sin(X) + cos(Y);
+% C = X.*Y;
+% surf(X,Y,Z,C)
+% colorbar
